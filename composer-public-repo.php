caf36a8d063b3ebe6439d57b6ea3b0ba62340cb3
Jason C <cauwels@usc.edu>
2021-01-12 17:19:09 -0800

    Create plugin file

---
+++ composer-public-repo.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Composer_Public_Repo
+ * @version 1.0.1
+ */
+/*
+Plugin Name: Composer Public Repo
+Plugin URI: http://github.com/uscwebservices/composer-public-repo
+Description: Test plugin for Composer Public Repo
+Author: USC ITS
+Version: 1.0.1
+*/
